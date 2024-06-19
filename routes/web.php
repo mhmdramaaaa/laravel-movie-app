dceca7902175bab4e32568b0eccf997d61b1060e
mhmdramaaaa <ramadani9743@gmail.com>
2024-06-19 10:15:03 +0700

    upload pertemuan 13

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MovieController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,26 +14,21 @@ Route::get('/', function () {
 
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/movies', function () {
-    return view('movies.index');
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/genres', GenreController::class);
+    Route::resource('/reviews', ReviewController::class);
 });
 
-Route::get('/create', function () {
-    return view('movies.create');
-});
 
 
 
-Route::resource('/movies', MovieController::class);
-
-Route::resource('/genres', GenreController::class);
-
-Route::get('/reviews', [ReviewController::class, 'index']);
-Route::get('/reviews/create', [ReviewController::class, 'create']);
-Route::post('/reviews', [ReviewController::class, 'store']);
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
-Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit']);
-Route::put('/reviews/{review}', [ReviewController::class, 'update']);
-
-
-
+require __DIR__.'/auth.php';
